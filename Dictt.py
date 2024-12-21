6571e8f018a626c482b8bdb42b4b92f82de4185e
Tusharch223 <111845971+Tusharch223@users.noreply.github.com>
2024-12-21 20:00:25 +0530

    Add files via upload

---
+++ Dictt.py
@@ -0,0 +1,11 @@
+def greet_user(name):
+    print(f'hi, good morning! {name} ')
+    print('what would you like to have? ')
+
+
+print("start")
+greet_user("tushar")
+greet_user("ankit")
+greet_user("sonia")
+greet_user("anu")
+print("finish")
